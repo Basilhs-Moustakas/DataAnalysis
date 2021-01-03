56963a0b5496abadf6e60d8d2d25f2e41f1e4ab2
BMoustakas <basilesmoustakas>
2021-01-03 19:16:14 +0200

    function added

---
+++ Maxima_difference_calc.m
@@ -0,0 +1,11 @@
+function difference_est = Maxima_difference_calc(country_data_confirmed,country_data_deaths)
+data=1:length(country_data_confirmed);
+pd = fitdist(data','lognormal','frequency',country_data_confirmed);
+[~,max_confirmed_day_est]=max(pdf(pd,data));
+data=1:length(country_data_deaths);
+pd = fitdist(data','lognormal','frequency',country_data_deaths);
+[~,max_deaths_day_est]=max(pdf(pd,data));
+difference_est=max_deaths_day_est-max_confirmed_day_est;
+
+end
+
