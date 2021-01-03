0526a1b3ea8d0658e73fbdb7d903a4780bd5ec16
BMoustakas <basilesmoustakas>
2021-01-03 16:18:24 +0200

    Bar graphs for mse

--- Corona_Plot.m
+++ Corona_Plot.m
@@ -1,4 +1,4 @@
-function [] = Corona_Plot(data,distribution,Title,array,country)
+function [] = Corona_Plot(data,distribution,Title,array,country,data_type)
 sum=0;
 for i=1:length(array)
     sum = sum+array(i);
@@ -9,9 +9,9 @@ y = pdf(pd,data);
 figure
 bar(data,normalized_array);
 hold on;
-plot(data,y, 'Color','magenta','LineWidth',2 );
-
-title(strcat(Title , country) , 'fontsize',17);
+p=plot(data,y, 'Color','magenta','LineWidth',2 );
+legend(p,Title)
+title(strcat( country, data_type) , 'fontsize',17);
 ylabel('Density', 'fontsize',14)
 xlabel('Day', 'fontsize',14)
 end
--- Group1Exe2Prog1.m
+++ Group1Exe2Prog1.m
@@ -10,35 +10,6 @@ country = country.country;
 
 
 %% Data from every Country
-% country{1,1} =  table2array(Confirmed(117,74:242));
-% country{1,2}    =  table2array(Deaths(117,74:242));
-% country{2,1} =  table2array(Confirmed(52,59:169));
-% country{2,2}    =  table2array(Deaths(52,59:169));
-% country{3,1} =  table2array(Confirmed(48,59:148));
-% country{3,2}    =  table2array(Deaths(48,59:148));
-% country{4,1} =  table2array(Confirmed(147,59:198));
-% country{4,2}    =  table2array(Deaths(147,59:198));
-% country{5,1} =  table2array(Confirmed(67,55:196));
-% country{5,2}    =  table2array(Deaths(67,55:196));
-% country{6,1} =  table2array(Confirmed(130,58:162));
-% country{6,2}    =  table2array(Deaths(130,58:162));
-% country{7,1} =  table2array(Confirmed(116,69:157));
-% country{7,2}    =  table2array(Deaths(116,69:157));
-% country{8,1} =  table2array(Confirmed(97,61:158));
-% country{8,2}    =  table2array(Deaths(97,61:158));
-% country{9,1} =  table2array(Confirmed(54,68:156));
-% country{9,2}    =  table2array(Deaths(54,68:156));
-% country{10,1} =  table2array(Confirmed(113,67:142));
-% country{10,2}    =  table2array(Deaths(113,67:142));
-% country{11,1} =  table2array(Confirmed(13,63:156));
-% country{11,2}    =  table2array(Deaths(13,63:156));
-% country{12,1} =  table2array(Confirmed(35,64:151));
-% country{12,2}    =  table2array(Deaths(35,64:151));
-% country{13,1} =  table2array(Confirmed(60,70:153));
-% country{13,2}    =  table2array(Deaths(60,70:153));
-% country{14,1} =  table2array(Confirmed(133,60:211));
-% country{14,2}    =  table2array(Deaths(133,60:211));
-
 
 distribution = 'lognormal';
 
@@ -67,48 +38,112 @@ country_errors = cell(length(strings_array),3);
 for i=1:length(strings_array)
     country_errors{i,1} = error(i,1);
     country_errors{i,2} = error(i,2);
-    country_errors{i,3} = strings_array(i);
 end
+country_errors{1,3}='Russia';
+country_errors{2,3}='Germany';
+country_errors{3,3}='France';
+country_errors{4,3}='UK';
+country_errors{5,3}='Italy';
+country_errors{6,3}='Spain';
+country_errors{7,3}='Romania';
+country_errors{8,3}='Netherlands';
+country_errors{9,3}='Greece';
+country_errors{10,3}='Portugal';
+country_errors{11,3}='Belgium';
+country_errors{12,3}='Czechia';
+country_errors{13,3}='Hungary';
+country_errors{14,3}='Sweden';
 
-sorted_confirmed = sortrows(country_errors,1)
-sorted_deaths = sortrows(country_errors,2)
+
+sorted_confirmed = sortrows(country_errors,1);
+sorted_deaths = sortrows(country_errors,2); 
+
+figure()
+X = categorical(sorted_confirmed(:,3));
+X = reordercats(X,sorted_confirmed(:,3));
+Y = cell2mat(sorted_confirmed(:,1));
+hb=bar(X,Y);
+my_colors=colormap(cool);
+hb.FaceColor = 'flat';
+hb.CData(1,:) = my_colors(60,:);
+hb.CData(2,:) = my_colors(56,:);
+hb.CData(3,:) = my_colors(52,:);
+hb.CData(4,:) = my_colors(48,:);
+hb.CData(5,:) = my_colors(44,:);
+hb.CData(6,:) = my_colors(40,:);
+hb.CData(7,:) = my_colors(36,:);
+hb.CData(8,:) = my_colors(32,:);
+hb.CData(9,:) = my_colors(28,:);
+hb.CData(10,:) = my_colors(24,:);
+hb.CData(11,:) = my_colors(20,:);
+hb.CData(12,:) = my_colors(16,:);
+hb.CData(13,:) = my_colors(12,:);
+hb.CData(14,:) = my_colors(8,:);
+
+title('Confirmed Cases','FontSize',16 ) 
+ylabel('Mean Square Error - Log Normal','FontSize',14 )
+grid on;
+
+
+figure()
+X = categorical(sorted_deaths(:,3));
+X = reordercats(X,sorted_deaths(:,3));
+Y = cell2mat(sorted_deaths(:,2));
+hb=bar(X,Y);
+my_colors=colormap(cool);
+hb.FaceColor = 'flat';
+hb.CData(1,:) = my_colors(60,:);
+hb.CData(2,:) = my_colors(56,:);
+hb.CData(3,:) = my_colors(52,:);
+hb.CData(4,:) = my_colors(48,:);
+hb.CData(5,:) = my_colors(44,:);
+hb.CData(6,:) = my_colors(40,:);
+hb.CData(7,:) = my_colors(36,:);
+hb.CData(8,:) = my_colors(32,:);
+hb.CData(9,:) = my_colors(28,:);
+hb.CData(10,:) = my_colors(24,:);
+hb.CData(11,:) = my_colors(20,:);
+hb.CData(12,:) = my_colors(16,:);
+hb.CData(13,:) = my_colors(12,:);
+hb.CData(14,:) = my_colors(8,:);
+
+title('Deaths','FontSize',16 ) 
+ylabel('Mean Square Error - Log Normal','FontSize',14 )
+grid on;
+
+
 
 
 %% Plots for every Country 
-Corona_Plot(data{1},'lognormal','Log Normal',country{1,1},' Russia');
-Corona_Plot(data{1},'lognormal','Log Normal',country{1,2},' Russia');
-Corona_Plot(data{2},'lognormal','Log Normal',country{2,1},' Germany');
-Corona_Plot(data{2},'lognormal','Log Normal',country{2,2},' Germany');
-Corona_Plot(data{3},'lognormal','Log Normal',country{3,1},' France');
-Corona_Plot(data{3},'lognormal','Log Normal',country{3,2},' France');
-Corona_Plot(data{4},'lognormal','Log Normal',country{4,1},' UK');
-Corona_Plot(data{4},'lognormal','Log Normal',country{4,2},' UK');
-Corona_Plot(data{5},'lognormal','Log Normal',country{5,1},' Italy');
-Corona_Plot(data{5},'lognormal','Log Normal',country{5,2},' Italy');
-Corona_Plot(data{6},'lognormal','Log Normal',country{6,1},' Spain');
-Corona_Plot(data{6},'lognormal','Log Normal',country{6,2},' Spain');
-Corona_Plot(data{7},'lognormal','Log Normal',country{7,1},' Romania');
-Corona_Plot(data{7},'lognormal','Log Normal',country{7,2},' Romania');
-Corona_Plot(data{8},'lognormal','Log Normal',country{8,1},' Netherlands');
-Corona_Plot(data{8},'lognormal','Log Normal',country{8,2},' Netherlands');
-Corona_Plot(data{9},'lognormal','Log Normal',country{9,1},' Greece');
-Corona_Plot(data{9},'lognormal','Log Normal',country{9,2},' Greece');
-Corona_Plot(data{10},'lognormal','Log Normal',country{10,1},' Portugal');
-Corona_Plot(data{10},'lognormal','Log Normal',country{10,2},' Portugal');
-Corona_Plot(data{11},'lognormal','Log Normal',country{11,1},' Belgium');
-Corona_Plot(data{11},'lognormal','Log Normal',country{11,2},' Belgium');
-Corona_Plot(data{12},'lognormal','Log Normal',country{12,1},' Czechia');
-Corona_Plot(data{12},'lognormal','Log Normal',country{12,2},' Czechia');
-Corona_Plot(data{13},'lognormal','Log Normal',country{13,1},' Hungary');
-Corona_Plot(data{13},'lognormal','Log Normal',country{13,2},' Hungary');
-Corona_Plot(data{14},'lognormal','Log Normal',country{14,1},' Sweden');
-Corona_Plot(data{14},'lognormal','Log Normal',country{14,2},' Sweden');
+% Corona_Plot(data{1},'lognormal','Log Normal',country{1,1},' Russia',' Confirmed Cases');
+% Corona_Plot(data{1},'lognormal','Log Normal',country{1,2},' Russia',' Deaths');
+% Corona_Plot(data{2},'lognormal','Log Normal',country{2,1},' Germany',' Confirmed Cases');
+% Corona_Plot(data{2},'lognormal','Log Normal',country{2,2},' Germany',' Deaths');
+% Corona_Plot(data{3},'lognormal','Log Normal',country{3,1},' France',' Confirmed Cases');
+% Corona_Plot(data{3},'lognormal','Log Normal',country{3,2},' France',' Deaths');
+% Corona_Plot(data{4},'lognormal','Log Normal',country{4,1},' UK',' Confirmed Cases');
+% Corona_Plot(data{4},'lognormal','Log Normal',country{4,2},' UK',' Deaths');
+% Corona_Plot(data{5},'lognormal','Log Normal',country{5,1},' Italy',' Confirmed Cases');
+% Corona_Plot(data{5},'lognormal','Log Normal',country{5,2},' Italy',' Deaths');
+% Corona_Plot(data{6},'lognormal','Log Normal',country{6,1},' Spain',' Confirmed Cases');
+% Corona_Plot(data{6},'lognormal','Log Normal',country{6,2},' Spain',' Deaths');
+% Corona_Plot(data{7},'lognormal','Log Normal',country{7,1},' Romania',' Confirmed Cases');
+% Corona_Plot(data{7},'lognormal','Log Normal',country{7,2},' Romania',' Deaths');
+% Corona_Plot(data{8},'lognormal','Log Normal',country{8,1},' Netherlands',' Confirmed Cases');
+% Corona_Plot(data{8},'lognormal','Log Normal',country{8,2},' Netherlands',' Deaths');
+% Corona_Plot(data{9},'lognormal','Log Normal',country{9,1},' Greece',' Confirmed Cases');
+% Corona_Plot(data{9},'lognormal','Log Normal',country{9,2},' Greece',' Deaths');
+% Corona_Plot(data{10},'lognormal','Log Normal',country{10,1},' Portugal',' Confirmed Cases');
+% Corona_Plot(data{10},'lognormal','Log Normal',country{10,2},' Portugal',' Deaths');
+% Corona_Plot(data{11},'lognormal','Log Normal',country{11,1},' Belgium',' Confirmed Cases');
+% Corona_Plot(data{11},'lognormal','Log Normal',country{11,2},' Belgium',' Deaths');
+% Corona_Plot(data{12},'lognormal','Log Normal',country{12,1},' Czechia',' Confirmed Cases');
+% Corona_Plot(data{12},'lognormal','Log Normal',country{12,2},' Czechia',' Deaths');
+% Corona_Plot(data{13},'lognormal','Log Normal',country{13,1},' Hungary',' Confirmed Cases');
+% Corona_Plot(data{13},'lognormal','Log Normal',country{13,2},' Hungary',' Deaths');
+% Corona_Plot(data{14},'lognormal','Log Normal',country{14,1},' Sweden',' Confirmed Cases');
+% Corona_Plot(data{14},'lognormal','Log Normal',country{14,2},' Sweden',' Deaths');
 
-
-
-
-
-%y = pdf(pd,data);
 
 
 
