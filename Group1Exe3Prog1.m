df7431a5ef0ec90a23942490aa921ff6fcb82981
BMoustakas <basilesmoustakas>
2021-01-03 19:19:27 +0200

    minor add

--- Group1Exe3Prog1.m
+++ Group1Exe3Prog1.m
@@ -3,5 +3,9 @@ clear all;
 
 country = load("country.mat");
 country = country.country;
-
-skata
+difference_maxima=zeros(1,14);
+for i=1:14
+    difference_maxima(i) = Maxima_difference_calc(country{i,1},country{i,2});
+    
+end
+difference_maxima
