80549635cdf98315f5842ea93b8ababf25ec7280
BMoustakas <basilesmoustakas>
2021-01-04 15:02:47 +0200

    this is right

--- Group1Exe5Prog1.m
+++ Group1Exe5Prog1.m
@@ -43,8 +43,8 @@ estimated_deaths = cell(21,6);
 for tau=0:20
 X = cell(length(strings_array),1);
     for i=1:length(strings_array)
-        confirmed = countries{i,1}(1+tau:end);
-        deaths = countries{i,2}(1:end-tau);
+        confirmed = countries{i,1}(1:end-tau);
+        deaths = countries{i,2}(1+tau:end);
         X{i} = [ones(length(confirmed'),1) confirmed'];
         b(:,i) = X{i}\deaths';
         estimated_deaths{tau+1,i} = X{i}*b(:,i);
@@ -55,7 +55,7 @@ errors = zeros(21,6);
 
 for i=1:length(strings_array)
     for tau=0:20
-        errors(tau+1,i) = immse(estimated_deaths{tau+1,i},countries{i,2}(1:end-tau)');
+        errors(tau+1,i) = immse(estimated_deaths{tau+1,i},countries{i,2}(1+tau:end)');
     end
 end
 
@@ -64,11 +64,13 @@ days_delay=zeros(1,6);
 for i=1:length(strings_array)
     [~,days_delay(i)] = min(errors(:,i));
 end
-% days_delay
-
-plot(countries{1,1}(1:end-18),estimated_deaths{19,1})
+days_delay=days_delay-1;
+days_delay
+tau=18;
+c=4;
+plot(countries{c,1}(1:end-tau),estimated_deaths{1+tau,c})
 hold on
-scatter(countries{1,1}(1:end-18),countries{1,2}(1:end-18))
+scatter(countries{c,1}(1:end-tau),countries{c,2}(1+tau:end))
 
 
 
