bd847c3607ef9008a0206d194529c61854381a9d
BMoustakas <basilesmoustakas>
2021-01-04 14:28:05 +0200

    this is wrong

--- Group1Exe5Prog1.m
+++ Group1Exe5Prog1.m
@@ -36,13 +36,49 @@ countries{6,2} = country{8,2};
 % for j=1:length(strings_array)  
 %     lambda(j) = countries{j,1}'\countries{j,2}';  
 % end
+
+estimated_deaths = cell(21,6);
+
+
+for tau=0:20
 X = cell(length(strings_array),1);
-for i=1:length(strings_array)
-    X{i} = [ones(length(countries{i,1}'),1) countries{i,1}'];
-    b(:,i) = X{i}\countries{i,2}';
+    for i=1:length(strings_array)
+        confirmed = countries{i,1}(1+tau:end);
+        deaths = countries{i,2}(1:end-tau);
+        X{i} = [ones(length(confirmed'),1) confirmed'];
+        b(:,i) = X{i}\deaths';
+        estimated_deaths{tau+1,i} = X{i}*b(:,i);
+    end
 end
 
-y1 = X{1}*b(:,1);
-plot(countries{1,1},y1)
+errors = zeros(21,6);
+
+for i=1:length(strings_array)
+    for tau=0:20
+        errors(tau+1,i) = immse(estimated_deaths{tau+1,i},countries{i,2}(1:end-tau)');
+    end
+end
+
+days_delay=zeros(1,6);
+
+for i=1:length(strings_array)
+    [~,days_delay(i)] = min(errors(:,i));
+end
+% days_delay
+
+plot(countries{1,1}(1:end-18),estimated_deaths{19,1})
 hold on
-scatter(countries{1,1},countries{1,2})
+scatter(countries{1,1}(1:end-18),countries{1,2}(1:end-18))
+
+
+
+
+
+
+
+
+
+
+
+
+
