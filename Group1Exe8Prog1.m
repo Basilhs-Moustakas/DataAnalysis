3eda63599f613961974e84f0967e253942a89b83
BMoustakas <basilesmoustakas>
2021-01-05 23:24:11 +0200

    Exercise 8 file added

---
+++ Group1Exe8Prog1.m
@@ -0,0 +1,27 @@
+close all;
+clear all;
+ 
+country = load("countries_second_wave.mat");
+countries = country.countries_second_wave;
+B = load("B1");
+B = B.B1;
+
+
+X = cell(6,1);
+estimated_deaths=cell(6,1);
+for i=1:6
+    deaths = countries{i,2}(1+20:end);
+    X{i} = ones(length(deaths),1);
+    for tau=0:20
+        X{i} = [X{i}  countries{i,1}(1+tau:end-20+tau)'];
+    end
+    estimated_deaths{i} = X{i}*B(:,i);
+end
+for i=1:6
+figure
+plot(countries{i,2}(1+20:end))
+hold on;
+plot(estimated_deaths{i}')
+grid on;
+hold off;
+end
